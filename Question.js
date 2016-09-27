d341fc0b5c7698e8167904d98e93137c72d1a082
tsatsimahase <tsatsi.mahase@gmail.com>
2016-09-27 20:15:33 +0200

    adding navigation

---
+++ Question.js
@@ -0,0 +1,84 @@
+import React, {PropTypes, Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Navigator,
+  TouchableOpacity,
+  ToolbarAndroid,
+  TouchableHighlight,
+  BackAndroid,
+} from 'react-native';
+
+import {Toolbar, Card, Button, Icon } from 'react-native-material-design';
+
+export default class Question extends Component {
+    static propTypes = {
+            title: PropTypes.string.isRequired,
+            onForward: PropTypes.func.isRequired,
+            onBack: PropTypes.func.isRequired
+        }
+    render(){
+        const questions = [
+          {
+            questionId:"0122",
+            title: "STYLE OF DELIVERY",
+            description: "Did the speaker use clear accesible language ?"
+          },
+          {
+            questionId:"0123",
+            title: "PRESENTATION LENGTH",
+            description: "Was the presentation an appropriate length ?"
+          }
+
+        ]
+        const renderedQuestions = questions.map((question)=>{
+          return <Card key={question.questionId}>
+                    <Card.Body>
+                        <Text> {question.description} </Text>
+                    </Card.Body>
+                    <Card.Actions position="right">
+                     <TouchableHighlight onPress={this.props.onForward}>
+                        <Text> Rate </Text>
+                     </TouchableHighlight>
+                    </Card.Actions>
+                        
+                </Card>
+        });
+
+    const toolBarIcon = this.props.index!=0 ? "arrow-back": "menu";
+    return (<View>
+      <Toolbar title={this.props.title} onIconPress={this.props.onBack} icon={toolBarIcon} style={styles.toolbar}/>
+          <ScrollView style={styles.list}>
+            {renderedQuestions}
+          </ScrollView>
+      </View>
+ )
+    }
+ }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+    list:{
+        marginTop:60
+    }
+  
+});
+
--- index.android.js
+++ index.android.js
@@ -11,66 +11,29 @@ import {
   ToolbarAndroid,
   BackAndroid,
 } from 'react-native';
+import Question from './Question';
 
-import {Toolbar, Card, Button } from 'react-native-material-design';
 class RateMyPrezi extends Component {
   render() {
-    const questions = [
-      {
-        questionId:"0122",
-        title: "STYLE OF DELIVERY",
-        description: "Did the speaker use clear accesible language ?"
-      },
-      {
-        questionId:"0123",
-        title: "PRESENTATION LENGTH",
-        description: "Was the presentation an appropriate length ?"
-      }
-
-    ]
-    const renderedQuestions = questions.map((question)=>{
-      return <Card key={question.questionId}>
-                <Card.Body>
-                    <Text> {question.description} </Text>
-                </Card.Body>
-                <Card.Actions position="right">
-                    <Button value="ACTION" text={"Rate"}/>
-                </Card.Actions>
-                    
-            </Card>
-    });
-
-    return (<View>
-      <Toolbar title={"Building an app with react native"} style={styles.toolbar}/>
-          <ScrollView style={styles.list}>
-            {renderedQuestions}
-          </ScrollView>
-      </View>
-    );
-  }
+      return (<Navigator initialRoute={{ title: 'My initial Scene', index:0}}
+              renderScene={(route, navigator) => { 
+                return  <Question onForward={()=> {
+                    const nextIndex = route.index + 1;
+                    navigator.push({
+                        title: 'Scene' + nextIndex,
+                        index: nextIndex,
+                    });
+                }} onBack = { () => {
+                    if( route.index > 0) {
+                        navigator.pop();
+                    }
+                }}
+                index={route.index}
+                title={route.title}/>
+  }}
+  />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-    list:{
-        marginTop:60
-    }
-  
-});
+}
 
 AppRegistry.registerComponent('RateMyPrezi', () => RateMyPrezi);
